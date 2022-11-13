75e694af500614bddf0c9973b03d7aa1bf583de3
trimonuter <mrqaist@gmail.com>
2022-11-13 11:54:16 +0700

    added pangkat(a, b) function to git_test.py

--- git_test.py
+++ git_test.py
@@ -1,2 +1,10 @@
-for i in range(5):
-  print("Hello World")
+def pangkat(a, b):
+  hasil = 1
+  for i in range(b):
+    hasil *= a
+  return hasil
+
+basis = int(input("Masukkan basis: "))
+eksp = int(input("Masukkan eksponen: "))
+
+print(f"Hasil pangkat: {pangkat(basis, eksp)}")
